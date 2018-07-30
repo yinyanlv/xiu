a58d360836c2bdb161e7933168912cb50a1bbe94
yinyanlv <yanlv.yin@gmail.com>
2018-07-30 15:34:55 +0800

    init project

---
+++ src/main/java/com/bugong/xiu/XiuApplication.java
@@ -0,0 +1,12 @@
+package com.bugong.xiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiuApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bugong/xiu/XiuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bugong.xiu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XiuApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
